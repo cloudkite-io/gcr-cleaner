f078688310573245ab7b564a1c61ca100ac404bf
KoomeKiriinya <ian@cloudkite.io>
2022-05-30 23:43:59 +0300

    add should delete function

--- pkg/gcrcleaner/gcr_cleaner.go
+++ pkg/gcrcleaner/gcr_cleaner.go
@@ -4,7 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
+
+	// "time"
 
 	"github.com/cloudkite-io/gcr-cleaner/pkg/config"
 	gcrauthn "github.com/google/go-containerregistry/pkg/authn"
@@ -64,6 +70,103 @@ func (gcrcleaner *GCRCleaner) GetKubernetesImages(ctx context.Context) []string 
 	return containers
 }
 
+func (gcrcleaner *GCRCleaner) shouldDelete(digest string, m google.ManifestInfo, repo string, since time.Time, tagFilter *regexp.Regexp, imageFilter *regexp.Regexp, clusterImages []string) bool {
+	// Immediately exclude images that have been uploaded after the given time.
+	if uploaded := m.Uploaded.UTC(); uploaded.After(since) {
+		fmt.Println("should not delete",
+			"repo", repo,
+			"digest", digest,
+			"reason", "too new",
+			"since", since.Format(time.RFC3339),
+			"uploaded", uploaded.Format(time.RFC3339),
+			"delta", uploaded.Sub(since).String())
+		return false
+	}
+
+	// If tagged images are allowed and the given filter matches the list of tags,
+	// this is a deletion candidate. The default tag filter is to reject all
+	// strings.
+	for _, tag := range m.Tags {
+		imagename := gcrcleaner.Config.CleanerConf.REGISTRY + "/" + repo + ":" + tag
+
+		if tagFilter.MatchString(tag) {
+			fmt.Println("Regex " + tagFilter.String() + " matches " + tag)
+			fmt.Println("should not delete",
+				"imagename", imagename,
+				"digest", digest,
+				"reason", "matches tag filter",
+				"tag_filter", tagFilter)
+			return false
+
+		}
+
+		if imageFilter.MatchString(imagename) {
+			fmt.Println("should not delete",
+				"imagename", imagename,
+				"digest", digest,
+				"reason", "matches images filter",
+				"tag", tag)
+			return false
+
+		}
+
+		if stringInSlice(imagename, clusterImages) {
+			fmt.Println("should not delete",
+				"imagename", imagename,
+				"digest", digest,
+				"reason", "image in use in clusters",
+				"tag", tag)
+			return false
+		}
+
+	}
+
+	// repo refers to
+
+	// If we got this far, it'ts not a viable deletion candidate.
+	fmt.Println("should delete",
+		"repo", repo,
+		"digest", digest,
+		"reason", "no filter matches")
+	return true
+
+}
+
+func (gcrcleaner *GCRCleaner) deleteOne(ctx context.Context, ref gcr.Reference) error {
+	if err := remote.Delete(ref,
+		remote.WithAuth(gcrcleaner.Auth),
+		remote.WithContext(ctx)); err != nil {
+		return fmt.Errorf("failed to delete %s: %w", ref, err)
+	}
+
+	return nil
+}
+
+func (gcrcleaner *GCRCleaner) Delete(ctx context.Context)(imagesToDelete ) {
+	var deleted = make([]string, 0, len(tags.Manifests))
+	var deletedLock sync.Mutex
+	var errs = make(map[string]error)
+	var errsLock sync.RWMutex
+
+	for _, tag := range m.Info.Tags {
+		c.logger.Debug("deleting tag",
+			"repo", repo,
+			"digest", m.Digest,
+			"tag", tag)
+
+		tagged := gcrrepo.Tag(tag)
+		if !dryRun {
+			if err := c.deleteOne(ctx, tagged); err != nil {
+				return nil, fmt.Errorf("failed to delete %s: %w", tagged, err)
+			}
+		}
+
+		deletedLock.Lock()
+		deleted = append(deleted, tagged.Identifier())
+		deletedLock.Unlock()
+	}
+}
+
 func (gcrcleaner *GCRCleaner) Clean(ctx context.Context) {
 	registry, err := gcr.NewRegistry(gcrcleaner.Config.CleanerConf.REGISTRY)
 	if err != nil {
@@ -74,8 +177,26 @@ func (gcrcleaner *GCRCleaner) Clean(ctx context.Context) {
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
 	}
-	containerimages := gcrcleaner.GetKubernetesImages(ctx)
 
+	imageFilter, err := regexp.Compile(gcrcleaner.Config.CleanerConf.OMIT_IMAGES_REGEX)
+	if err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+
+	tagFilter, err := regexp.Compile(gcrcleaner.Config.CleanerConf.OMIT_TAGS_REGEX)
+	if err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+
+	ageDays, err := strconv.Atoi(gcrcleaner.Config.CleanerConf.AGE_DAYS)
+	if err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+	since := time.Now().AddDate(0, 0, -ageDays)
+
+	clusterImages := gcrcleaner.GetKubernetesImages(ctx)
+
+	// imagesToDelete := []string{}
 	for _, repo := range repos {
 		if strings.Contains(repo, gcrcleaner.Config.CleanerConf.PROJECT_ID) {
 			gcrrepo, err := gcr.NewRepository(gcrcleaner.Config.CleanerConf.REGISTRY + "/" + repo)
@@ -83,21 +204,19 @@ func (gcrcleaner *GCRCleaner) Clean(ctx context.Context) {
 				log.Fatalf("ERROR: %s", err)
 			}
 
-			tags, err := google.List(gcrrepo,
+			imageinfo, err := google.List(gcrrepo,
 				google.WithContext(ctx),
 				google.WithAuth(gcrcleaner.Auth))
 			if err != nil {
-				log.Fatalf("ERRORx: %s", err)
+				log.Fatalf("ERROR: %s", err)
 			}
-			for _, manifest := range tags.Manifests {
-				for _, tag := range manifest.Tags {
-					if stringInSlice(gcrcleaner.Config.CleanerConf.REGISTRY+"/"+repo+":"+tag, containerimages) {
-						fmt.Println("Dont delete:" + gcrcleaner.Config.CleanerConf.REGISTRY + "/" + repo + ":" + tag)
-					} else {
-						fmt.Println("Delete: " + gcrcleaner.Config.CleanerConf.REGISTRY + "/" + repo + ":" + tag)
+			for digest, manifest := range imageinfo.Manifests {
 
-					}
+				to_delete := gcrcleaner.shouldDelete(digest, manifest, repo, since, tagFilter, imageFilter, clusterImages)
+				if to_delete {
+
 				}
+
 			}
 
 		}
